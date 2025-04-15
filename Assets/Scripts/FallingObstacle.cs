87b58b1fa3ed5c376e5166bf777758ccc8e953c8
MarkEthanFowler <methanfowler@gmail.com>
2025-04-15 17:39:19 -0400

    Remove Leaf Logic
    Added a coroutine that deletes the game object.

--- Assets/Scripts/FallingObstacle.cs
+++ Assets/Scripts/FallingObstacle.cs
@@ -6,16 +6,25 @@ public class FallingObstacle : MonoBehaviour
 {
 
     [SerializeField] private Transform obstacle;
+    private WaitForSeconds removeLeafObjectTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        removeLeafObjectTime = new WaitForSeconds(10f);
+        StartCoroutine(RemoveLeafObjectTimer());
     }
 
     // Update is called once per frame
     void Update()
     {
         obstacle.position -= new Vector3 (0f, .01f, 0f);
+    }
+
+    public IEnumerator RemoveLeafObjectTimer()
+    {
+        yield return removeLeafObjectTime;
+        Destroy(this);
+        Destroy(gameObject);
     }
 }
